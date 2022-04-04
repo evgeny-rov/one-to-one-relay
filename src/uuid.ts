dc94596a0a8a4e326783018acb728d7593bdfd32
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-04-04 22:10:22 +0300

    restructured project

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,12 +1,14 @@
-export default (length = 11) => {
-  const CHARS_SOURCE =
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+export const uuid = () => {
+  const CHARS_SOURCE = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+  const LENGTH = 11;
 
   let uid = '';
 
-  for (let i = 0; i < length; i += 1) {
+  for (let i = 0; i < LENGTH; i += 1) {
     uid += CHARS_SOURCE[Math.floor(Math.random() * CHARS_SOURCE.length)];
   }
 
   return uid;
 };
+
+export const validateUid = (uid: string) => /^[\w-]{11,11}$/.test(uid);
