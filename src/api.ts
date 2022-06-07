35f936ed94db3e8893d5c1e784a4d3f3276f2686
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-06-07 17:17:21 +0800

    reduced sid length, specified close messages and codes

--- src/api.ts
+++ src/api.ts
@@ -4,46 +4,76 @@ import { validateCommand, formatAsCommand } from './command';
 
 import type { CommandName, Command } from './command';
 
+const closeMessagesByCode = {
+  4001: 'Connection timed out',
+  4002: 'Received incorrect or unknown command',
+  4003: 'Connection closed by peer',
+  4004: 'Session not found',
+  4005: 'Unable to create new session',
+} as const;
+
 const waitingRoom = new Map<string, WebSocket>();
 const wss = new WebSocketServer({ port: 8080 });
 
-const cleanup = (id: string) => waitingRoom.delete(id);
+const getUnusedSessionId = (retries = 100): string | null => {
+  if (retries <= 0) {
+    return null;
+  }
 
-const handleClose = (reason: string, ...clients: [WebSocket, ...WebSocket[]]) => {
-  clients.forEach((ws) => ws.close(1000, reason));
+  const id = uuid();
+
+  if (!waitingRoom.has(id)) {
+    return id;
+  }
+
+  return getUnusedSessionId(retries - 1);
+};
+
+const clearSessionId = (id: string) => waitingRoom.delete(id);
+
+const handleClose = (
+  closeCode: keyof typeof closeMessagesByCode,
+  ...clients: [WebSocket, ...WebSocket[]]
+) => {
+  clients.forEach((ws) => ws.close(closeCode, closeMessagesByCode[closeCode]));
 };
 
 const commandResolvers: Record<CommandName, (ws: WebSocket, command: Command) => void> = {
   create: (ws) => {
-    const id = uuid();
-    waitingRoom.set(id, ws);
-    ws.on('close', () => cleanup(id));
-    ws.send(`created ${id}`);
+    const id = getUnusedSessionId();
+
+    if (id) {
+      waitingRoom.set(id, ws);
+      ws.onclose = () => clearSessionId(id);
+      ws.send(`created ${id}`);
+    } else {
+      handleClose(4005, ws);
+    }
   },
   connect: (ws, command) => {
     const [, id] = command;
     const peer = waitingRoom.get(id);
 
     if (peer) {
-      cleanup(id);
+      clearSessionId(id);
       const peers = [ws, peer];
 
       peers.forEach((p, idx, arr) => {
         const nextPeer = arr[(idx + 1) % arr.length];
 
-        p.on('close', () => handleClose('Peer closed connection', nextPeer));
+        p.onclose = () => handleClose(4003, nextPeer);
         p.on('message', (data) => nextPeer.send(data.toString()));
         p.send('connected');
       });
     } else {
-      handleClose('Not found', ws);
+      handleClose(4004, ws);
     }
   },
 };
 
 const handleConnection = (ws: WebSocket) => {
   const TIMEOUT_AFTER = 30000;
-  const timerId = setTimeout(() => handleClose('Timed out', ws), TIMEOUT_AFTER);
+  const timerId = setTimeout(() => handleClose(4001, ws), TIMEOUT_AFTER);
 
   const handleMessage = (rawData: RawData) => {
     clearTimeout(timerId);
@@ -54,7 +84,7 @@ const handleConnection = (ws: WebSocket) => {
       const [commandName] = someCommand;
       commandResolvers[commandName](ws, someCommand);
     } else {
-      handleClose('Unknown or incorrect command', ws);
+      handleClose(4002, ws);
     }
   };
 
